f63f2ac8578b06aeedac9a7e9a5da6a0148ba478
vboxuser <vboxuser@FossalFoca.myguest.virtualbox.org>
2025-02-14 09:52:56 -0600

    first commit

---
+++ build/bumperbot_example/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vboxuser/bumperbot_ws/src/bumperbot_example
+# Build directory: /home/vboxuser/bumperbot_ws/build/bumperbot_example
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
